3d508f9fc19b8ab8487be26edcfbd9dc12a7e077
Jeffry Almonte Dely <djempsly120@gmail.com>
2024-09-20 07:35:50 -0400

    Haciendo los primeros commits

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// Array para almacenar los productos en el carrito
+let carrito = [];
+
+// Función para agregar un producto al carrito
+function agregarAlCarrito(productoId) {
+    carrito.push(productoId);
+    alert('Producto agregado al carrito.');
+    // Actualiza la interfaz del carrito si es necesario
+}
